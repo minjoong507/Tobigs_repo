d2383e805e3504dd57ce861659fed0e228550fbd
albert507 <albert507@naver.com>
2020-01-28 16:49:28 +0900

    Task1 과제 완료

--- 2_week/Regression/HW_Task1.py
+++ 2_week/Regression/HW_Task1.py
@@ -3,20 +3,17 @@ from numpy.linalg import inv
 
 
 def estimate_beta(x, y):
-    beta_hat = []
-    for x_list in x:
-        x_list.append(1)
+    new_x = inv(np.dot(x.T, x))
+    new_y = np.dot(x.T, y)
+    beta_hat = np.dot(new_x, new_y)
 
     return beta_hat
 
 
 x = np.array([[1, 0, 1],
-               [1, 2, 3],
-               [1, 3, 8]])
+              [1, 2, 3],
+              [1, 3, 8]
+              ])
 
 y = np.transpose(np.array([1, 3, 7]))
-
-for x_list in x:
-    x_list = np.append(x_list, [1])
-    print(x_list)
-print(x)
+print(estimate_beta(x, y))
