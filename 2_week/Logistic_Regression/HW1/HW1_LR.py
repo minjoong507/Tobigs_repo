45684d2f364148d8dd952250e145e0d2a98b87df
minjoong <albert507@naver.com>
2020-02-01 14:17:46 +0900

    로지스틱 1번과제 제출

--- 2_week/Logistic_Regression/HW1/HW1_LR.py
+++ 2_week/Logistic_Regression/HW1/HW1_LR.py
@@ -23,12 +23,35 @@ X_test_s = ss.transform(X_test)
 
 logreg = LogisticRegression()
 logreg.fit(X_train_s, y_train)
-logreg.score(X_test_s, y_test)
+print(logreg.score(X_test_s, y_test))
+
+fpr, tpr, thresholds = roc_curve(y_test, logreg.predict_proba(X_test_s)[:, 1])
+x = fpr
+y = tpr
+
+plt.plot(x, y)
+plt.xlabel('1-Specificity')
+plt.ylabel('Sensitivity')
+plt.show()
+res = [[abs(fpr_i - tpr_i), thres_i] for fpr_i, tpr_i, thres_i in zip(fpr, tpr, thresholds)]
+print(res)
+d_point = 0
+for i in res:
+    if i[0] > d_point:
+        d_point = i[0]
+        cut_off = i[1]
+print(cut_off)
+
 
 # confusion matrix
-y_pred = logreg.predict(X_test)
-conf_m = confusion_matrix(y_pred, y_test)
-print(conf_m)
+y_pred = logreg.predict_proba(X_test_s)[:,1:]
+a = []
+for i in y_pred:
+    if i < cut_off:
+        a.append(0)
+    else:
+        a.append(1)
+conf_m = confusion_matrix(a, y_test)
 a = conf_m[0][0] + conf_m[1][1]
 sum = 0
 for row in conf_m:
@@ -36,15 +59,3 @@ for row in conf_m:
        sum += index
 
 print(a/sum)
-
-fpr, tpr, thresholds = roc_curve(y_test, logreg.decision_function(X_test_s))
-x = fpr
-y = tpr
-
-plt.plot(x,y) # 간단하게 ROC 그려볼 수 있음.
-plt.xlabel('1-Specificity')
-plt.ylabel('Sensitivity')
-plt.show()
-print(thresholds)
-res = [[fpr_i, tpr_i, thres_i] for fpr_i, tpr_i, thres_i in zip(fpr, tpr, thresholds)]
-print(res)
