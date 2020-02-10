b54350735f1a3bfb85bee393408e3821dc68d002
albert507 <albert507@naver.com>
2020-02-10 23:24:38 +0900

    앙상블 2번과제 코드파일

--- 4_week/ensemble/HW_ensemble#2.py
+++ 4_week/ensemble/HW_ensemble#2.py
@@ -3,7 +3,19 @@ import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import matplotlib.pyplot as plt  # Matlab-style plotting
 import seaborn as sns
 from sklearn.preprocessing import StandardScaler
-
+from sklearn.preprocessing import LabelEncoder
+from sklearn.linear_model import ElasticNet, Lasso,  BayesianRidge, LassoLarsIC
+from sklearn.ensemble import RandomForestRegressor,  GradientBoostingRegressor
+from sklearn.kernel_ridge import KernelRidge
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import RobustScaler
+from sklearn.base import BaseEstimator, TransformerMixin, RegressorMixin, clone
+from sklearn.model_selection import KFold, cross_val_score, train_test_split
+from sklearn.metrics import mean_squared_error
+import xgboost as xgb
+import lightgbm as lgb
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
 color = sns.color_palette()
 sns.set_style('darkgrid')
 
@@ -28,7 +40,9 @@ test.drop("Id", axis = 1, inplace = True)
 #check again the data size after dropping the 'Id' variable
 print("\nThe train data size after dropping Id feature is : {} ".format(train.shape))
 print("The test data size after dropping Id feature is : {} ".format(test.shape))
-
+ntrain = train.shape[0]
+print(ntrain)
+print("))))")
 fig, ax = plt.subplots()
 ax.scatter(x = train['SalePrice'], y = train['GrLivArea'])
 plt.ylabel('SalePrice', fontsize=13)
@@ -88,8 +102,50 @@ all_data["LotFrontage"] = all_data.groupby("Neighborhood")["LotFrontage"].transf
 # for i in all_data["BsmtQual"].isnull():
 #     if i is True:
 #         print("fuck")
-all_data['MSSubClass'] = all_data['MSSubClass'].to_string()
-print(all_data['OverallCond'])
+# all_data['MSSubClass'] = all_data['MSSubClass'].astype(str)
+all_data['MSSubClass'] = all_data['MSSubClass'].astype(str)
+
+print(all_data['MSSubClass'])
+#MSSubClass=The building class
+all_data['MSSubClass'] = all_data['MSSubClass'].astype(str)
+
+
+#Changing OverallCond into a categorical variable
+all_data['OverallCond'] = all_data['OverallCond'].astype(str)
+
+
+#Year and month sold are transformed into categorical features.
+all_data['YrSold'] = all_data['YrSold'].astype(str)
+all_data['MoSold'] = all_data['MoSold'].astype(str)
+all_data['FireplaceQu'] = all_data['FireplaceQu'].astype(str)
+cols = ('FireplaceQu', 'BsmtQual', 'BsmtCond', 'GarageQual', 'GarageCond',
+        'ExterQual', 'ExterCond','HeatingQC', 'PoolQC', 'KitchenQual', 'BsmtFinType1',
+        'BsmtFinType2', 'Functional', 'Fence', 'BsmtExposure', 'GarageFinish', 'LandSlope',
+        'LotShape', 'PavedDrive', 'Street', 'Alley', 'CentralAir', 'MSSubClass', 'OverallCond',
+        'YrSold', 'MoSold')
+for col in cols:
+    LE = LabelEncoder()
+    print(all_data[col])
+    print(col)
+    all_data[col] = LE.fit_transform(all_data[col])
+    print(all_data[col])
+
+# shape
+print(all_data['FireplaceQu'])
+print('Shape all_data: {}'.format(all_data.shape))
 
 
 
+X_train, X_val, y_train, y_val = train_test_split(train, test, random_state=49)
+
+gbrt = GradientBoostingRegressor(max_depth=2, n_estimators=120, random_state=42)
+gbrt.fit(X_train, y_train)
+
+# 최적의 트리 개수 찾기
+errors = [mean_squared_error(y_val, y_pred)
+          for y_pred in gbrt.staged_predict(X_val)]
+bst_n_estimators = np.argmin(errors)
+
+# 최적의 트리개수로 그래디언트 부스팅 학습
+gbrt_best = GradientBoostingRegressor(max_depth=2, n_estimators=bst_n_estimators, random_state=42)
+gbrt_best.fit(X_train, y_train)
